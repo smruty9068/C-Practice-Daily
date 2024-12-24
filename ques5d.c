faf37552e366a5b3b89af7cc4f5ba507b8ef24aa
smruti9068 <113612132+smruti9068@users.noreply.github.com>
2024-12-25 01:45:49 +0530

    Delete ques5d.c

--- ques5d.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int sum;
-    sum = 2 + 4 / 2 + 6 * 2;
-    printf("%d", sum);
-    return 0;
-}
-
-// Output will be 16. Because / and * has higher presidence than +, so these will be calculated first with left to right associativity.

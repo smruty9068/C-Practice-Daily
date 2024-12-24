c1ed2226e27a8747b50b184d743488a924bed0a4
smruti9068 <113612132+smruti9068@users.noreply.github.com>
2024-12-25 01:45:02 +0530

    Delete ques5a.c

--- ques5a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x = -123;
-    printf("%u", x);
-    return (0);
-}
-
-// %u is unsigned integer, hence it will return the 32bit (2's complement of -123) - 123 in decimal format.

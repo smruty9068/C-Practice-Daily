e2df9bdc0ae7d471d5150cc44a5ddce1c535ef06
smruti9068 <113612132+smruti9068@users.noreply.github.com>
2024-12-26 16:23:49 +0530

    Delete a1q2.c

--- a1q2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> // Preprocessor directive : Used to import different libraries.
-
-int main() { // Main function: Program execution starts from here.
-    int num; // Variable named num which can store data of integer type.
-    scanf("%d",&num); // Scan function: Used to take user input.
-    printf("%s", "Debadarshi Omkar"); // Print function: Used to display output. "%s" is a format specifier which represents the type of data.
-    return 0; // return statement: At the end of the every function return statement returns the answer accordingly.
-}

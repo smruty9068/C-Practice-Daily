22b756c8d580762175dcb9aa318030fda3179431
smruti9068 <113612132+smruti9068@users.noreply.github.com>
2024-12-26 16:26:47 +0530

    Delete a1q5e.c

--- a1q5e.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-    char ch = 97;
-    printf("%d\n", sizeof(ch + 4));
-}
-
-// Output will be 4 because sizeof function is used for an arithmetic operation which comprises of one int and one char. Since the size of int is greater than char, the output of the operation will be 101 which is a int of size 4.
